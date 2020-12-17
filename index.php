b6535f533b2d9fe5f5285d84ec548bdb9ba37c8e
Gitdevnew <75690051+Gitdevnew@users.noreply.github.com>
2020-12-17 13:09:15 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+ <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="CSS/main2.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+        <title>Accueil Groupement Banque-Assurance Français (GBAF).</title>
+    </head>
+<body>
+  <div id="logo">
+               <a href="index.php" title= "Page accueil"> <img src="Images/LOGO_GBAF.png" alt="logo" style="width: 100px;"></a>
+  </div>
+
+<div class="container">
+<h2>  Extranet du Groupement Banque-Assurance Français (GBAF)</h2>
+
+<button class="buttonaccueil"
+        type="button">
+   <a href="connexion.php">Allez à la page de connexion</a>
+</button>
+
+</div>
+
+<?php
+
+include("Commun/footer.php");
+ ?>
+</body>
+</html>
