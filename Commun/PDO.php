57664f0c26f89c298fc40c360a57214766bddd32
Robert Guerroua <robert.guerroua@gmail.com>
2020-12-17 13:45:58 +0100

    connexion à la bdd

---
+++ Commun/PDO.php
@@ -0,0 +1,11 @@
+<?php
+
+ try
+{
+  $bdd = new PDO('mysql:host=localhost;dbname=proj3oc;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(PDOException $e)
+{
+     $message = "Erreur PDO avec le message : " . $e->getMessage();
+     die($message);
+}
