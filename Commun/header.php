ceacd920d2a5cc6b4ddf0b1790fc4fea63ccbb33
Gitdevnew <75690051+Gitdevnew@users.noreply.github.com>
2020-12-17 13:09:55 +0100

    Add files via upload

---
+++ Commun/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <?php
+            if(!empty($title))
+            {
+        ?>
+        <title><?= $title; }?></title>
+        <link rel="stylesheet" type="text/css" href="CSS/main2.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+
+            <a href="index.php">
+                <div id="logo">
+                    <img class="logo" src="Images/LOGO_GBAF.png" alt="logo" style="height: 100px;">
+                </div>
+            </a>
+
+
+            </div>
+        </header>
