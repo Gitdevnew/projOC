15b5eb6fc77ece027cdd7545704f13d0f6bda52c
Robert Guerroua <robert.guerroua@gmail.com>
2020-12-17 13:20:43 +0100

    footer

---
+++ Commun/footer.php
@@ -0,0 +1,13 @@
+
+
+<footer>
+        <div class="containfoot">
+
+                <p>Projet 3 Formation initiation Fullstack : extranet d'un groupe bancaire.
+                <a href="mentionlegales.php">Mentions légales</a>
+                <a href="mailto:contact@gbaf.com">Contact</a></p>
+
+          </div>
+      </footer>
+  </body>
+</html>
