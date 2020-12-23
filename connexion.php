0db7409e6de76a9418deb8e5ea8d427bdc0a8dd6
Robert Guerroua <robert.guerroua@gmail.com>
2020-12-23 14:08:35 +0100

    Rajout du code

--- connexion.php
+++ connexion.php
@@ -1,6 +1,6 @@
 
 <?php
-        //initialisation de la variable title ajouté au header
+        // initialisation de la variable title ajoutéau header
         $title = 'Page connexion utilisateur';
         include("Commun/header.php");
 ?>
@@ -10,17 +10,44 @@
     <div id="corps">
       <div id="connect">
         <h2>    Connectez vous</h2>
+
         <!-- Formulaire de connexion -->
         <div class="formulaire">
           <form method="post" action="verification_connexion.php">
+            <fieldset>
+            <legend>Utilisateur</legend>
             <label for="pseudo">Votre pseudo </label> <br>
             <input class="input" type="text" name="username" id="pseudo"> <br>
             <label for="mdp">Votre mot de passe </label> <br>
             <input class="input" type="password" name="password" id="mdp"><br>
             <input class="btn_connexion" type="submit" value="Connexion"> <br><br>
             <a href="password_oublie.php"> Vous avez oublié votre mot de passe ? </a><br><br>
+            </fieldset>
           </form>
         </div>
+
+        <?php
+
+        //affiche une erreur si mdp faux envoyé par la page verification_connexion
+        if(!empty($_GET['err']) && $_GET['err']== "password")
+        {
+          echo '<p style="color: #F51720;"><strong> Mot de passe ou pseudo incorrect ! </strong></p>';
+        }
+
+        // affiche une erreur si tous les champs ne sont pas remplis envoyé par la page verification_connexion
+        if(!empty($_GET['err']) && $_GET['err']== "champs")
+        {
+          echo '<p style="color: #F51720;"><strong>Veuillez remplir tous les champs.</strong></p>';
+        }
+
+        // affiche une validation si mdp modifié envoyé par la page nouveau_mot_de_passe
+        if(!empty($_GET['ok']) && $_GET['ok']== "password")
+        {
+          echo '<p style="color: lightgreen;"><strong> Votre mot de passe a bien été modifié ! </strong> </p>';
+        }
+        ?>
+
+
         <div class="new_u">
           <p> Nouvel utilisateur ?<br>
             <button class="btn_connexion" onclick= "window.location.href ='inscription.php';">Inscrivez vous</button>
@@ -29,8 +56,8 @@
       </div>
     </div>
 <!-- FOOTER -->
-<?php
+        <?php
 
-    include("Commun/footer.php");
- ?>
+        include("Commun/footer.php");
+        ?>
 
