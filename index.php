068db67e152103c6f867e2d50295cc05eb03a98b
Robert Guerroua <robert.guerroua@gmail.com>
2020-12-24 10:33:40 +0100

    modifications classes css

--- index.php
+++ index.php
@@ -16,11 +16,16 @@
 
 <div class="container">
 <h2>  Extranet du Groupement Banque-Assurance Français (GBAF)</h2>
-
-<button class="buttonaccueil"
-        type="button">
+<div class="buttons">
+  <button class="btn-hover color-8">
    <a href="connexion.php">Allez à la page de connexion</a>
-</button>
+  </button>
+</div>
+<div class="buttons">
+  <p> Nouvel utilisateur ?<br>
+    <button class="btn-hover color-11" onclick= "window.location.href ='inscription.php';">Inscrivez vous</button>
+  </p>
+</div>
 
 </div>
 
