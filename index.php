2638d12071f23953c917b3a5f261c06d992d1f06
Robert Guerroua <robert.guerroua@gmail.com>
2021-01-05 12:51:51 +0100

    réindentation

--- index.php
+++ index.php
@@ -9,29 +9,28 @@
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
         <title>Accueil Groupement Banque-Assurance Français (GBAF).</title>
     </head>
-<body>
-  <div id="logo">
-               <a href="index.php" title= "Page accueil"> <img src="Images/LOGO_GBAF.png" alt="logo" style="width: 100px;"></a>
-  </div>
+    <body>
+      <div id="logo">
+                   <a href="index.php" title= "Page accueil"> <img src="Images/LOGO_GBAF.png" alt="logo" style="width: 100px;"></a>
+      </div>
 
-<div class="container">
-<h2>  Extranet du Groupement Banque-Assurance Français (GBAF)</h2>
-<div class="buttons">
-  <button class="btn-hover color-8">
-   <a href="connexion.php">Allez à la page de connexion</a>
-  </button>
-</div>
-<div class="buttons">
-  <p> Nouvel utilisateur ?<br>
-    <button class="btn-hover color-11" onclick= "window.location.href ='inscription.php';">Inscrivez vous</button>
-  </p>
-</div>
+      <div class="container">
+        <h2>  Extranet du Groupement Banque-Assurance Français (GBAF)</h2>
+        <div class="buttons">
+          <button class="btn-hover color-8">
+           <a href="connexion.php">Allez à la page de connexion</a>
+          </button>
+        </div>
+        <div class="buttons">
+          <p> Nouvel utilisateur ?<br>
+            <button class="btn-hover color-11" onclick= "window.location.href ='inscription.php';">Inscrivez vous</button>
+          </p>
+        </div>
 
-</div>
+      </div>
 
-<?php
-
-include("Commun/footer.php");
- ?>
-</body>
-</html>
+      <?php
+      include("Commun/footer.php");
+       ?>
+    </body>
+  </html>
