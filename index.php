b1c8be58e0fe12eee435ff01df57bd7d53bc2bb5
Robert Guerroua <robert.guerroua@gmail.com>
2021-01-08 19:22:42 +0100

    refonte de la page

--- index.php
+++ index.php
@@ -1,36 +1,26 @@
-<!DOCTYPE html>
- <html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="CSS/main2.css">
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-        <title>Accueil Groupement Banque-Assurance Français (GBAF).</title>
-    </head>
-    <body>
-      <div id="logo">
-                   <a href="index.php" title= "Page accueil"> <img src="Images/LOGO_GBAF.png" alt="logo" style="width: 100px;"></a>
-      </div>
 
-      <div class="container">
-        <h2>  Extranet du Groupement Banque-Assurance Français (GBAF)</h2>
-        <div class="buttons">
-          <button class="btn-hover color-8">
-           <a href="connexion.php">Allez à la page de connexion</a>
-          </button>
-        </div>
-        <div class="buttons">
-          <p> Nouvel utilisateur ?<br>
-            <button class="btn-hover color-11" onclick= "window.location.href ='inscription.php';">Inscrivez vous</button>
-          </p>
+<?php
+// initialisation de la variable title ajoutéau header
+$title = 'Accueil Groupement Banque-Assurance Français (GBAF)';
+include("Commun/header.php");
+?>
+
+        <div class="container">
+          <h2>  Extranet du Groupement Banque-Assurance Français (GBAF)</h2>
+          <div class="buttons">
+            <button class="btn-hover color-8">
+             <a href="connexion.php">Allez à la page de connexion</a>
+            </button>
+          </div>
+          <div class="buttons">
+            <p> Nouvel utilisateur ?<br>
+              <button class="btn-hover color-11" onclick= "window.location.href ='inscription.php';">Inscrivez vous</button>
+            </p>
+          </div>
+
         </div>
 
-      </div>
+<?php
+include("Commun/footer.php");
+ ?>
 
-      <?php
-      include("Commun/footer.php");
-       ?>
-    </body>
-  </html>
