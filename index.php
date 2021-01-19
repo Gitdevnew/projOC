45f64c70aa7d17785e467c89903aaf76a4cff92c
Robert Guerroua <robert.guerroua@gmail.com>
2021-01-19 08:26:02 +0100

    Fichier final

--- index.php
+++ index.php
@@ -5,16 +5,19 @@ $title = 'Accueil Groupement Banque-Assurance Français (GBAF)';
 include("Commun/header.php");
 ?>
 
+
         <div class="container">
-          <h2>  Extranet du Groupement Banque-Assurance Français (GBAF)</h2>
+
+         <h1 >  Extranet du Groupement Banque-Assurance Français (GBAF)</h1>
+
           <div class="buttons">
-            <button class="btn-hover color-8">
-             <a href="connexion.php">Allez à la page de connexion</a>
-            </button>
+            <p> Allez à la page de connexion<br>
+              <button class="btn_connexion btn-hover color-8" onclick= "window.location.href ='connexion.php';">Connexion</button>
+            </p>
           </div>
           <div class="buttons">
             <p> Nouvel utilisateur ?<br>
-              <button class="btn-hover color-11" onclick= "window.location.href ='inscription.php';">Inscrivez vous</button>
+              <button class="btn_connexion btn-hover color-11" onclick= "window.location.href ='inscription.php';">Inscrivez vous</button>
             </p>
           </div>
 
