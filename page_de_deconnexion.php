bd41bc306556a0561a45b3e272b091ac1e8ce2a6
Robert Guerroua <robert.guerroua@gmail.com>
2021-01-19 08:39:30 +0100

    Fichier final

--- page_de_deconnexion.php
+++ page_de_deconnexion.php
@@ -1,13 +1,18 @@
 <?php
 session_start();
+
 // Suppression des variables
+
 unset($_SESSION['prenom']);
-unset($_SESSION['nom']);
 $_SESSION = array();
+
 // Suppression de la session
 session_destroy();
+
 // Suppression des cookies
 setcookie('login', '');
 setcookie('pass_hache', '');
 header('location: index.php');
+
+
 ?>
