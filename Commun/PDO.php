81b2ba37434d5e34d894966a0ec8cb9da5d861fc
Robert Guerroua <robert.guerroua@gmail.com>
2021-01-19 08:59:05 +0100

    Fichier final

--- Commun/PDO.php
+++ Commun/PDO.php
@@ -1,8 +1,11 @@
 <?php
-try {
+
+ try
+{
   $bdd = new PDO('mysql:host=localhost;dbname=proj3oc;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
-catch(PDOException $e) {
+catch(PDOException $e)
+{
      $message = "Erreur PDO avec le message : " . $e->getMessage();
      die($message);
 }
