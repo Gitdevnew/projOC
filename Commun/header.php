6b2b740ac1e7e3b74175e520ca0d1eb3a8075390
Robert Guerroua <robert.guerroua@gmail.com>
2021-01-19 08:58:04 +0100

    Fichier final

--- Commun/header.php
+++ Commun/header.php
@@ -22,11 +22,11 @@ if(empty($_SESSION['id_user']) AND empty($_SESSION['pseudo']) AND empty($_SESSIO
     </head>
     <body>
         <header>
-            <div>
 
-                <a href="index.php">
-                    <div id="logo">
-                        <img class="logo" src="Images/LOGO_GBAF.png" alt="logo" style="height: 100px;">
+            <div>
+            <a href="index.php">
+                    <div id="logo_accueil">
+                        <img class="logo_accueil" src="Images/LOGO_GBAF.png" alt="logo" >
                     </div>
                     <br><br>
                 </a>
@@ -54,23 +54,22 @@ else {
         </head>
         <body>
             <header>
-
-                 <a href="page_utilisateur.php">
-                    <span>
-                        <img  src="Images/LOGO_GBAF.png" alt="logo" style="height: 80px;">
-                    </span>
+                <a href="page_utilisateur.php">
+                <div id="logo_connecte">
+                    <img class="logo_connecte" src="Images/LOGO_GBAF.png" alt="logo">
+                </div>
                 </a>
                 <div id="affichage_nom">
                    <!-- affichage des données utilisateurs dans le header récupéré par la super globale $_session -->
-                    <p>
+                      <button class="nom_prenom">
                         <?php
-                            echo "Bienvenue  " . $_SESSION['nom'] .' '. $_SESSION['prenom'] . " !";
+                            echo "Bienvenue  " . "<br>" . $_SESSION['nom'] .' '. $_SESSION['prenom'] . " !";
                         ?>
-                    </p>
-                    <!-- affichage des liens vers les parametres du compte utilisateur et page de deconnexion quand l'utilisateur est connecté -->
-                    <button  class=" btn_connexion buttons btn-hover color-8"  onclick= "window.location.href = 'page_des_parametres_utilisateur.php';"> Paramètres Utilisateur
                     </button>
-                    <button class="btn_connexion  buttons btn-hover color-11" onclick= "window.location.href = 'page_de_deconnexion.php';"> Déconnexion
+                    <!-- affichage des liens vers les parametres du compte utilisateur et page de deconnexion quand l'utilisateur est connecté -->
+                    <button  class="bouton_header relative"  onclick= "window.location.href = 'page_des_parametres_utilisateur.php';"> Paramètres Utilisateur
+                    </button>
+                    <button class="bouton_header relative" onclick= "window.location.href = 'page_de_deconnexion.php';"> Déconnexion
                     </button>
                 </div>
             </header>
