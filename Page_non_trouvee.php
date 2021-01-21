d39dd1f8d77e2c4fc27474dbac57d52dc3b17953
Gitdevnew <75690051+Gitdevnew@users.noreply.github.com>
2021-01-21 23:18:03 +0100

    Add files via upload

---
+++ Page_non_trouvee.php
@@ -0,0 +1,18 @@
+
+<?php
+ $title = 'Page non trouvée';
+        include("Commun/header.php");
+ ?>
+ <!DOCTYPE html>
+   <html>
+     <head>
+       <title></title>
+     </head>
+     <body>
+      <div><br>
+       <h3> La page que vous demandez n'existe pas ! erreur 404. </h3>
+       <h4> Cliquez sur le logo pour retourner à l'accueil.</h4>
+      </div>
+     </body>
+   </html>
+
---
+++ acces_non_autorise.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Page accès non autorisé</title>
+    </head>
+    <body>
+      <div><br>
+        <h2>Accès interdit !</h2><br>
+        <h3> Vous n'avez pas le droit d'accéder à l'objet demandé. Soit celui-ci est protégé, soit il ne peut être lu par le serveur ! erreur 403. </h3>
+      </div>
+    </body>
+  </html>
+
