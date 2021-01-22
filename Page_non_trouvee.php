2b8bcec2803cad9ca6e0f7ed4b12f0f8b5c0806d
Gitdevnew <75690051+Gitdevnew@users.noreply.github.com>
2021-01-22 19:59:36 +0100

    Add files via upload

--- Page_non_trouvee.php
+++ Page_non_trouvee.php
@@ -3,16 +3,11 @@
  $title = 'Page non trouvée';
         include("Commun/header.php");
  ?>
- <!DOCTYPE html>
-   <html>
-     <head>
-       <title></title>
-     </head>
-     <body>
-      <div><br>
+
+      <div style="text-align: center; background: #cf1e10;"><br>
        <h3> La page que vous demandez n'existe pas ! erreur 404. </h3>
        <h4> Cliquez sur le logo pour retourner à l'accueil.</h4>
       </div>
-     </body>
-   </html>
-
+     <?php
+include("Commun/footer.php");
+?>
--- acces_non_autorise.php
+++ acces_non_autorise.php
@@ -1,13 +1,21 @@
 <!DOCTYPE html>
   <html>
     <head>
-      <title>Page accès non autorisé</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Page accès non autorisé erreur 403</title>
+      <link rel="stylesheet" type="text/css" href="CSS/main2.css">
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     </head>
     <body>
-      <div><br>
+      <div style="text-align: center; background: #cf1e10;"><br>
         <h2>Accès interdit !</h2><br>
         <h3> Vous n'avez pas le droit d'accéder à l'objet demandé. Soit celui-ci est protégé, soit il ne peut être lu par le serveur ! erreur 403. </h3>
+        <button  class="bouton_header btn_connexion relative"  onclick= "window.location.href = 'index.php';"> Retour à l'accueil
+        </button>
       </div>
-    </body>
-  </html>
+<?php
+include("Commun/footer.php");
+?>
 
